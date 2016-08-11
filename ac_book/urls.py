bcda538a2337b382946e94f6abdf332b0bb24532
rtef23 <rtef23@gmail.com>
2016-08-11 18:48:04 +0900

    update. make consume_detail, consume_new

--- ac_book/urls.py
+++ ac_book/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.consume_list, name='consume_list'),
+	url(r'^consume/(?P<pk>[0-9]+)$', views.consume_detail, name='consume_detail'),
+	url(r'^consume/new/$', views.consume_new, name='consume_new'),
 ]
