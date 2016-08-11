0cd2b7e69f8516887321173d59a95707609931ee
ErickRyu <rkggmdii@gmail.com>
2016-08-11 15:32:41 +0900

    신규. Consume 모델을 만들다

---
+++ ac_book/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import Consume
+
+admin.site.register(Consume)
---
+++ ac_book/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AcBookConfig(AppConfig):
+    name = 'ac_book'
