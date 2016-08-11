44007aef2d7cc5e7949d6e5282f84c47a2e3d29a
rtef23 <rtef23@gmail.com>
2016-08-11 19:18:44 +0900

    update. enroll consume_edit url which should be connected to views.consume_edit

--- ac_book/urls.py
+++ ac_book/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
 	url(r'^$', views.consume_list, name='consume_list'),
 	url(r'^consume/(?P<pk>[0-9]+)$', views.consume_detail, name='consume_detail'),
 	url(r'^consume/new/$', views.consume_new, name='consume_new'),
+	url(r'^consume/(?P<pk>[0-9]+)/edit/$', views.consume_edit, name = 'consume_edit'),
 ]
