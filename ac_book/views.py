85da70280424a86c99a018c5b8ceb15527a01bbc
rtef23 <rtef23@gmail.com>
2016-08-11 18:48:33 +0900

    update. make consume_detail, consume_new

--- ac_book/views.py
+++ ac_book/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
 from .models import Consume
 from django.utils import timezone
+from .forms import ConsumeForm
+
+from django.conf.urls import include
+
 
 def consume_list(request):
 	consumes = Consume.objects.order_by('con_date')
 	return render(request, 'ac_book/consume_list.html', {'consumes' : consumes})
+
+def consume_detail(request, pk):
+	consume = get_object_or_404(Consume, pk=pk)
+	return render(request, 'ac_book/consume_detail.html', {'consume':consume})
+
+def consume_new(request):
+	if request.method == "POST":
+		form = ConsumeForm(request.POST)
+		if form.is_valid():
+			consume = form.save(commit=False)
+			consume.save()
+			return redirect('consume_detail', pk=consume.pk)
+	else:
+		form = ConsumeForm()
+	return render(request, 'ac_book/consume_edit.html', {'form':form})
