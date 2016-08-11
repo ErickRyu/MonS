ee5908516bdaab5e43593d3fbdcf05e52bd6386f
rtef23 <rtef23@gmail.com>
2016-08-11 18:47:06 +0900

    new. create consume form

---
+++ ac_book/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Consume
+
+class ConsumeForm(forms.ModelForm):
+	class Meta:
+		model = Consume
+		fields = ('con_type', 'store_name', 'con_price', 'con_date')
